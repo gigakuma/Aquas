7043ec20b1322d76d98f57c3e75615efb7fe0b9b
Qiang <qiang.l.x@gmail.com>
2015-02-16 13:29:30 +0800

    Initial Commit

---
+++ Aquas/Aquas.h
@@ -0,0 +1,19 @@
+//
+//  Aquas.h
+//  Aquas
+//
+//  Created by Qiang on 2/16/15.
+//  Copyright (c) 2015 gigakuma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Aquas.
+FOUNDATION_EXPORT double AquasVersionNumber;
+
+//! Project version string for Aquas.
+FOUNDATION_EXPORT const unsigned char AquasVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Aquas/PublicHeader.h>
+
+
